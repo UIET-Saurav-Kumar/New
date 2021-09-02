becad10c642398696886afb3952f1a0c8411290a
vinender <vinendersingh91@gmail.com>
2021-09-02 17:00:33 +0530

    build error resolved

--- shop-rest/src/pages/registerShop.tsx
+++ shop-rest/src/pages/registerShop.tsx
@@ -4,7 +4,7 @@ import HeroHome from '@components/shop-home-page/hero-home';
 import FeaturesBlocks from '@components/shop-home-page/features-blocks';
 import Footer2 from '@components/shop-home-page/footer2';
 import Features from '@components/shop-home-page/features';
-// import '/css/styles.scss';
+
 
 function registerShop() {
 
@@ -12,22 +12,19 @@ function registerShop() {
       
     <div className="flex flex-col min-h-screen overflow-hidden">
 
-      {/*  Site header */}
       <Header />
 
-      {/*  Page content */}
       <main className="flex-grow">
 
-        {/*  Page sections */}
+     
         <HeroHome />
         <Features/>
         <FeaturesBlocks />
-        {/* <Testimonials /> */}
-        {/* <Newsletter /> */}
+     
 
       </main>
 
-      {/*  Site footer */}
+  
       <Footer2 />
 
     </div>
