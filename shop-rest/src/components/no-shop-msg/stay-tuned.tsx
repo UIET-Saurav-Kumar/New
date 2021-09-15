8f01a1df4d6f9e59810cdaa1908a7cd961180444
vinender <vinendersingh91@gmail.com>
2021-09-15 15:48:41 +0530

    merged latest changes and resolved build errors

--- shop-rest/src/components/no-shop-msg/stay-tuned.tsx
+++ shop-rest/src/components/no-shop-msg/stay-tuned.tsx
@@ -7,9 +7,11 @@ export default function StayTuned() {
             <h1 className='text-5xl text-red-700  font-bold lg:text-8xl tracking-widest '>Stay </h1>
             <h1 className='text-5xl text-blue-600 font-bold lg:text-8xl tracking-widest' >Tuned</h1>
             <p className=' w:1/2 px-4 lg:w-1/3 text-center font-semibold text-lg items-center flex'>
-                    We couldn’t find any restaurants near your location that we currently deliver from. 
+                 Sorry we couldn’t find any restaurants near your location that we currently deliver from. 
                     Follow along as we launch in new cities.
             </p>
+
+            <h2 className='text-gray-500 text-md '> Currently we are operating in <span className='font-semibold'>Chandigarh, Panchkula and Mohali.</span> </h2>
             
         </div>
     )
