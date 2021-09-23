579d44e6ec57a2d60ef6b900992dc773d67d331c
vinender <vinendersingh91@gmail.com>
2021-09-23 20:00:26 +0530

    product card changes

--- shop-rest/src/components/delivery/address-input.tsx
+++ shop-rest/src/components/delivery/address-input.tsx
@@ -2,11 +2,14 @@ import Input from "@components/ui/input";
 import Button from "@components/ui/button";
 import TextArea from "@components/ui/text-area";
 import { useTranslation } from "next-i18next";
+import { useState } from "react";
 
 
-export default function ShopPaymentForm() {
-  
+
+export default function DeliveryAddressInputs() {
   const { t } = useTranslation("common") ;
+  const [rider, setRider] = useState(false);
+
 
 
     return (
@@ -17,25 +20,26 @@ export default function ShopPaymentForm() {
                             <div className="mt-0 w-full  h-full p-4 flex-col flex space-y-6">
 
                                   <Input
-                                    label={t("Enter amount")}
-                                    type='number'
-                                    placeholder=' ₹'
+                                    // label={t("Enter amount")}
+                                    
+                                    placeholder='Pickup location'
                                     // {...register("name")}
                                     variant="outline"
                                     // error={t(errors.name?.message!)}
                                   />
 
-                                  <TextArea
-                                    label={t("Product description")}
+                                  <Input
+                                    // label={t("Product description")}
                                     // {...register("description")}
+                                    placeholder='Drop location'
                                     variant="outline"
                                     className="-my-2"
                                     rows={2}
                                     // error={t(errors.description?.message!)}
                                   />
 
-                                <Button >
-                                  {t("Pay Now")}
+                                <Button>
+                                  {t("Book Delivery")}
                                 </Button>
 
                             </div>
