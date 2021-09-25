ecc615bc9f7f8995b881befae058cdf646bf2e3f
vinender <vinendersingh91@gmail.com>
2021-09-25 18:28:43 +0530

    create shop ui

--- admin-rest/src/components/dashboard/owner.tsx
+++ admin-rest/src/components/dashboard/owner.tsx
@@ -5,6 +5,9 @@ import Image from "next/image";
 import { useMeQuery } from "@data/user/use-me.query";
 import ShopCard from "@components/shop/shop-card";
 import NoShopSvg from "../../../public/no-shop.svg";
+import { ROUTES } from "@utils/routes";
+import LinkButton from "@components/ui/link-button";
+
 
 export default function OwnerDashboard() {
   const { t } = useTranslation();
@@ -27,19 +30,68 @@ export default function OwnerDashboard() {
       </div>
 
       {!data?.managed_shop && !data?.shops?.length ? (
-        <div className="w-full flex flex-col items-center p-10">
-          <div className="w-[300px] sm:w-[490px] h-[180px] sm:h-[370px] relative">
-            <Image
-              alt={t("common:text-image")}
-              src={NoShopSvg}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <span className="text-lg font-semibold text-center text-body-dark mt-6 sm:mt-10">
-            {t("common:text-no-shop")}
-          </span>
+
+        <div className=' flex py-10 lg:py-10  justify-around pt-0 items-start overflow-y-hidden shadow-sm space-y-8 h-11/12 w-full  opacity-75'>
+         
+        
+
+                {/* <div className='bg-yellow-400 rounded-lg h-80 w-72
+                                hover:transform translate-x-4 translate-y-4'></div> */}
+
+                <div className='flex flex-col space-y-4 sm:space-y-8 lg:space-y-10'> 
+                    <h1 className='text-center text-2xl lg:text-4xl  mt-10 font-extrabold font-serif '>
+                      Take Your Business to Millions of Customers Near You
+                    <p className='text-xl lg:text-2xl mt-10 tracking-widest font-light text-gray-700 px-4 md:px-20 lg:px-20 xl:px-60 text-center'>
+                    Create your shop, build an active and engaged audience for your business
+                    </p>
+                    </h1> 
+
+                    <img 
+                src='https://bluerayws.com/sites/default/files/2019-12/E-COMMERCE-%26-ONLINE-SHOPPING-SERVICES_2.gif'
+                // src='https://previews.123rf.com/images/zuperia/zuperia2003/zuperia200300413/142389355-online-shopping-concept-couple-man-and-woman-are-shopping-online-man-is-carrying-cart-with-purchases.jpg' 
+                className=' w-36 h-36 mx-auto object-contain sm:hidden '
+                />
+                    <p className='text-center text-gray-700'>  </p>
+
+                   <div className='mx-auto'> 
+                      <LinkButton  href={ROUTES.CREATE_SHOP} 
+                         className='  text-lg text-white font-semibold lg:text-xl px-6 p-4 rounded-md bg-green-700'>
+                         Create Shop now
+                      </LinkButton>
+                   </div>
+
+                    
+                </div>
+
+          
+
+          <img 
+                src='https://bluerayws.com/sites/default/files/2019-12/E-COMMERCE-%26-ONLINE-SHOPPING-SERVICES_2.gif'
+                // src='https://previews.123rf.com/images/zuperia/zuperia2003/zuperia200300413/142389355-online-shopping-concept-couple-man-and-woman-are-shopping-online-man-is-carrying-cart-with-purchases.jpg' 
+                className='hidden sm:block object-contain lg:w-80 lg:h-80 '
+                />
+         
+
+
         </div>
+
+        // <div className="w-full flex flex-col items-center p-10">
+        //   <div className="w-[800px] sm:w-[800px] h-[180px] sm:h-[370px] relative">
+
+         
+        //     <Image
+        //       alt={t("common:text-image")}
+        //       src={NoShopSvg}
+        //       layout="fill"
+        //       objectFit="cover"
+        //     />
+
+        //   </div>
+        //   <span className="text-lg font-semibold text-center text-body-dark mt-6 sm:mt-10">
+        //     {t("common:text-no-shop")}
+        //   </span>
+        // </div>
+
       ) : null}
       {!!data?.managed_shop ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5 gap-5">
