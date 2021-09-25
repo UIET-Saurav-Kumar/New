6e1ae11b1b973332ef64a2a133899597c32a1cde
vinender <vinendersingh91@gmail.com>
2021-09-25 18:56:38 +0530

    create shop ui, payment gateway

--- api/config/cashfree.php
+++ api/config/cashfree.php
@@ -13,10 +13,15 @@ return [
     //live: appID: 13353224f34e6b8d5dec4c7c13235331
     //live: secretKey: 5cc5c4adb74168906d10b82bf7820a69dc23634a
     'PG' => [
-        'appID' => '86476b28568df28cc6b9dc11567468',
-        'secretKey' => 'e437a64b56d62b71638e8dd4957e268409a7b6ba',
+        //  live keys
+        'appID' => '13353224f34e6b8d5dec4c7c13235331',
+        'secretKey' => '5cc5c4adb74168906d10b82bf7820a69dc23634a',
+
+        //  test keys 
+        // 'appID' => '86476b28568df28cc6b9dc11567468',
+        // 'secretKey' => 'e437a64b56d62b71638e8dd4957e268409a7b6ba',
         'testURL' => 'https://test.cashfree.com',
         'prodURL' => 'https://api.cashfree.com',
-        'isLive' => false
+        'isLive' => true
     ]
 ];
