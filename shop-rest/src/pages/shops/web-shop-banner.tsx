01f6d79e3f2bb033c440ba9833c3ccb8c76a2960
vinender singh <46550815+vinender@users.noreply.github.com>
2021-10-05 17:27:26 +0530

    Update web-shop-banner.tsx

--- shop-rest/src/pages/shops/web-shop-banner.tsx
+++ shop-rest/src/pages/shops/web-shop-banner.tsx
@@ -3,7 +3,7 @@ export default function WebShopBanner() {
 
     return (
 
-        <div  className='no-scrollbar flex justify-between space-x-2 w-full h-34   mt-2  overflow-x-scroll'>
+        <div  className='no-scrollbar flex justify-between space-x-2 w-full h-34   mt-4  overflow-x-scroll'>
 
             <div className=' flex justify-center items-center rounded-lg space-x-16  lg:px-2 px-2 w-full h-28 lg:h-36 xl:h-42 bg-red-500 '>
                     <img src='/rupee.png' className='object-contain h-20 w-20 lg:h-28 lg:w-28'/>
