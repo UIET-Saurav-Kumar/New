24ca7f6f069554687cc26259b24f865486e4f74b
vinender singh <46550815+vinender@users.noreply.github.com>
2021-10-06 15:27:13 +0530

    Update order-items-table.tsx

--- shop-rest/src/components/order/order-items-table.tsx
+++ shop-rest/src/components/order/order-items-table.tsx
@@ -3,12 +3,43 @@ import usePrice from "@utils/use-price";
 import { siteSettings } from "@settings/site.settings";
 import { useTranslation } from "next-i18next";
 import { useIsRTL } from "@utils/locals";
+import { useOrderQuery } from "@data/order/use-order.query";
+import { useRouter } from "next/router";
+
 
 export const OrderItems = ({ products }: { products: any }) => {
   const { t } = useTranslation("common");
   const { alignLeft, alignRight } = useIsRTL();
+  // const { items, isEmpty } = useCart();
+  const { query } = useRouter();
+
+
+
+  const { data, isLoading: loading } = useOrderQuery({
+    tracking_number: query.tracking_number as string,
+  });
+
+  const { price: total } = usePrice(data && { amount: data.order.paid_total });
+  const { price: sub_total } = usePrice(data && { amount: data.order.amount });
+  const { price: shipping_charge } = usePrice(
+    data && { amount: data?.order?.delivery_fee ?? 0 }
+  );
+
+  const { price: tax } = usePrice(
+    data && { amount: data?.order?.sales_tax ?? 0 }
+  );
+
+  const { price: discount } = usePrice(
+    data && { amount: data?.order?.discount ?? 0 }
+  );
+
+
+  // var converter = require('number-to-words');
+
+  
 
   const orderTableColumns = [
+
     {
       title: <span className="ps-20">{t("text-item")}</span>,
       dataIndex: "",
@@ -17,18 +48,24 @@ export const OrderItems = ({ products }: { products: any }) => {
       align: alignLeft,
       ellipsis: true,
       render: (_: any, record: any) => {
+
         const { price } = usePrice({
           amount: +record.pivot?.unit_price,
         });
+
         let name = record.name;
+
         if (record.pivot?.variation_option_id) {
           const variationTitle = record.variation_options?.find(
             (vo: any) => vo.id === record.pivot.variation_option_id
           )["title"];
           name = `${name} - ${variationTitle}`;
         }
+
         return (
+          <>
           <div className="flex items-center">
+
             <div className="w-16 h-16 flex flex-shrink-0 rounded overflow-hidden">
               <img
                 src={
@@ -53,10 +90,15 @@ export const OrderItems = ({ products }: { products: any }) => {
                 {price}
               </span>
             </div>
+
+            
           </div>
+        </>
         );
       },
     },
+
+
     {
       title: t("text-quantity"),
       dataIndex: "pivot",
@@ -67,8 +109,44 @@ export const OrderItems = ({ products }: { products: any }) => {
         return <p className="text-body">{pivot.order_quantity}</p>;
       },
     },
+  
+    
     {
-      title: t("text-price"),
+      title: t("Shipping Charge"),
+      dataIndex: "pivot",
+      key: "pivot",
+      align: "center",
+      width: 100,
+      render: () => {
+        return <p className="text-body">{shipping_charge}</p>;
+      },
+    },
+
+
+    {
+      title: t("Tax"),
+      dataIndex: "pivot",
+      key: "pivot",
+      align: "center",
+      width: 100,
+      render: () => {
+        return <p className="text-body">{tax}</p>;
+      },
+    },
+
+    {
+      title: t("Discount"),
+      dataIndex: "pivot",
+      key: "pivot",
+      align: "center",
+      width: 100,
+      render: () => {
+        return <p className="text-body">{discount}</p>;
+      },
+    },
+
+    {
+      title: t("Sub Total"),
       dataIndex: "pivot",
       key: "price",
       align: alignRight,
@@ -77,23 +155,40 @@ export const OrderItems = ({ products }: { products: any }) => {
         const { price } = usePrice({
           amount: +pivot.subtotal,
         });
-        return <p>{price}</p>;
+        return <p className=''>{price}</p>;
       },
     },
+
   ];
 
   return (
-    <Table
-      //@ts-ignore
-      columns={orderTableColumns}
-      data={products}
-      rowKey={(record: any) =>
-        record.pivot?.variation_option_id
-          ? record.pivot.variation_option_id
-          : record.created_at
-      }
-      className="orderDetailsTable w-full"
-      scroll={{ x: 350, y: 500 }}
-    />
+
+    <div className='border'>
+        <Table
+          //@ts-ignore
+          columns={orderTableColumns}
+          data={products}
+          rowKey={(record: any) =>
+            record.pivot?.variation_option_id
+              ? record.pivot.variation_option_id
+              : record.created_at
+          }
+          className="orderDetailsTable w-full"
+          scroll={{ x: 350, y: 500 }}
+        />
+
+              <p className=" p-2   flex  w-full text-body-dark items-center ">
+                <div className='flex ml-4 w-full '><strong className="w-5/12 sm:w-4/12   text-lg font-bold">
+                  {t("text-total")}
+                </strong>
+                :<span className=" items-center justify-end  mr-4 flex w-7/12 sm:w-8/12 ps-4 font-bold text-lg ">{total}</span></div>
+              </p>
+
+              {/* <div className='w-full flex border-t items-center h-20'>
+                <h1 className='font-semibold w-1/3 h-full text-center items-center justify-center flex border border-r text-lg'>Amount in Words</h1>
+                <p></p>
+              </div> */}
+    </div>
+    
   );
 };
