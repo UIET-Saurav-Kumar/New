1caa88e511c76fbc37f672c8b5197e3a308e56a2
vinender singh <46550815+vinender@users.noreply.github.com>
2021-10-13 15:49:22 +0530

    Update shop-description.tsx

--- shop-rest/src/pages/shops/shop-description.tsx
+++ shop-rest/src/pages/shops/shop-description.tsx
@@ -4,110 +4,108 @@ import { formatAddress } from "@utils/format-address";
 import { getIcon } from "@utils/get-icon";
 import * as socialIcons from "@components/icons/social";
 import ReadMore from "@components/ui/truncate";
+import Truncate from "@components/ui/truncate-scroll";
+import { scroller, Element } from "react-scroll";
 
 
 
 
 export default function ShopDescription({data}) {
 
+ 
+
     const { t } = useTranslation("common") ;
 
     return (
       
-        <div className=' hidden lg:inline-flex  bg-white justify-between xl:space-x-8 px-2 xl:px-16 mt-1  ;
+        <div className=' hidden lg:inline-flex  bg-white space-x-6 justify-between xl:space-x-8 px-2 xl:px-16 mt-1  ;
                          text-10px lg:text-sm pt-4 '>
 
-            <div className=' flex flex-col  '>
-
+             {/* Description */}
+            <div className=' flex flex-col  w-full text-left'>
                 <span className="text-lg text-heading font-semibold mb-2">
                       Description
                 </span>
 
                 { data?.description && (
                   
-                        <p className="text-lg font-md font-serif tracking-widest text-gray-600 w-24 lg:w-72 lg+:80 xl:w-96 xl+:96 mb-2 leading-relaxed">
-                            <ReadMore character={70}>{data?.description}</ReadMore>
+                        <p className="text-sm font-md  font-light tracking-wide text-gray-600  mb-2 leading-relaxed">
+                          <ReadMore character={70}>{data?.description}</ReadMore>
                         </p>
                 )}
-
             </div>  
 
 {/* <div className="p-4 flex justify-evenly  space-x-5"> */}
 
+              {/* Address */}
+              <div className="mb-7 last:mb-0 flex flex-col  w-full text-left">
+                  <span className="text-lg text-heading font-semibold mb-2">
+                    {t("text-address")}
+                  </span>
 
- <div className="mb-7 last:mb-0 flex flex-col">
+                  <span className="text-sm font-light tracking-wide  text-gray-600 ">
 
-     <span className="text-lg text-heading font-semibold mb-2">
-       {t("text-address")}
-     </span>
+                      {!isEmpty(formatAddress(data?.address))
+                    ? formatAddress(data?.address)
+                    : t("common:text-no-address")}
 
-     <span className="text-lg font-serif tracking-widest text-gray-600 w-28 xl:w-52">
+                  </span>
+              </div>
+              
+              {/* Contaact number */}
+              <div className="mb-7 last:mb-0 flex flex-col  w-full text-left">
+                  <span className="text-lg text-heading font-semibold mb-2">
+                    {t("Whatsapp/Call to order")}
+                  </span>
 
-         {!isEmpty(formatAddress(data?.address))
-       ? formatAddress(data?.address)
-       : t("common:text-no-address")}
+                  <span className="text-sm font-light tracking-wide text-body">
+                    {data?.settings?.contact
+                      ? t("77430-42380")
+                      : t("77430-42380")}
+                  </span>
+                  
+                  {/* <div className='flex 2xl:hidden '> 
 
-     </span>
+                      <a
+                          href={data?.settings?.website}
+                          target="_blank"
+                          className="text-md text-accent  bg-gray-100 rounded-md px-2 p-2 mt-4 
+                          font-bold hover:text-accent-hover focus:outline-none focus:text-accent-hover"
+                        >
+                          {t("text-visit-site")}
+                      </a>
+                  </div> */}
+              </div>
 
- </div>
+ {/* {data?.settings?.website && (
 
- <div className="mb-7 last:mb-0 flex flex-col">
+   <div className="flex items-left flex-col">
 
-     <span className="text-lg text-heading font-semibold mb-2">
-       {t("Whatsapp/Call to order")}
-     </span>
-
-     <span className="text-lg font-serif tracking-widest text-body">
-       {data?.settings?.contact
-         ? t("77430-42380")
-         : t("77430-42380")}
-     </span>
-     
-      
-
-     {/* <div className='flex 2xl:hidden '> 
-
-         <a
-             href={data?.settings?.website}
-             target="_blank"
-             className="text-md text-accent border bg-gray-100 rounded-md px-2 p-2 mt-4 
-             font-bold hover:text-accent-hover focus:outline-none focus:text-accent-hover"
-           >
-             {t("text-visit-site")}
-         </a>
-     </div> */}
-
- </div>
-
- {data?.settings?.website && (
-
-   <div className="flex items-center flex-col">
-
-       {/* <span className="text-sm text-heading font-semibold mb-2">
+       <span className="text-sm text-heading font-semibold mb-2">
          {t("text-website")}
-       </span> */}
+       </span>
 
        <div className = " " >
 
-         {/* <div className="text-sm border w-36 border-red-800  ">
+         <div className="text-sm  w-36 -red-800  ">
            {data?.settings?.website}
-         </div> */}
+         </div>
 
-         {/* <div className=' '> 
+         <div className=' '> 
 
              <a
                href={data?.settings?.website}
                target="_blank"
-               className=" hidden 2xl:block text-md text-accent border bg-gray-100 rounded-md px-2 p-2  font-bold hover:text-accent-hover focus:outline-none focus:text-accent-hover"
+               className=" hidden 2xl:block text-md text-accent  bg-gray-100 rounded-md px-2 p-2  font-bold hover:text-accent-hover focus:outline-none focus:text-accent-hover"
              >
                {t("text-visit-site")}
              </a>
 
-         </div> */}
+         </div>
 
        </div>
    </div>
- )}
+ )} */}
 
 </div>
     )
