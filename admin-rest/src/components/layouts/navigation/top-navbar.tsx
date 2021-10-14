d6d65117cd641579fc92c7e975023838c0744e3a
vinender singh <46550815+vinender@users.noreply.github.com>
2021-10-14 15:37:57 +0530

    Update top-navbar.tsx

--- admin-rest/src/components/layouts/navigation/top-navbar.tsx
+++ admin-rest/src/components/layouts/navigation/top-navbar.tsx
@@ -1,74 +1,46 @@
-import Logo from "@components/ui/logo";
-import { useUI } from "@contexts/ui.context";
-import AuthorizedMenu from "./authorized-menu";
-import Drawer from "@components/ui/drawer";
-import LinkButton from "@components/ui/link-button";
-import { NavbarIcon } from "@components/icons/navbar-icon";
-import { motion } from "framer-motion";
-import DrawerWrapper from "@components/ui/drawer-wrapper";
+import { useRouter } from "next/router";
+import Navbar from "@components/layouts/navigation/top-navbar";
+import { getAuthCredentials, hasAccess } from "@utils/auth-utils";
+import SidebarItem from "@components/layouts/navigation/sidebar-item";
 import { siteSettings } from "@settings/site.settings";
 import { useTranslation } from "next-i18next";
-import { ROUTES } from "@utils/routes";
-import {
-  adminAndOwnerOnly,
-  getAuthCredentials,
-  hasAccess,
-} from "@utils/auth-utils";
-import SidebarItem from "./sidebar-item";
 
-const Navbar = () => {
+const ShopLayout: React.FC = ({ children }) => {
+  
   const { t } = useTranslation();
-  const { toggleSidebar, displaySidebar, closeSidebar } = useUI();
+  const {
+    query: { shop },
+  } = useRouter();
 
-  const { permissions } = getAuthCredentials();
+  const { permissions: currentUserPermissions } = getAuthCredentials();
 
   return (
-    <header className="bg-white shadow fixed w-full z-40">
-      <nav className="px-5 md:px-8 py-4 flex items-center justify-between">
-        {/* <!-- Mobile menu button --> */}
-        <motion.button
-          whileTap={{ scale: 0.88 }}
-          onClick={toggleSidebar}
-          className="flex p-2 h-full items-center justify-center focus:outline-none focus:text-primary lg:hidden"
-        >
-          <NavbarIcon />
-        </motion.button>
-
-        <div className="hidden md:flex ms-5 me-auto">
-          <Logo />
-        </div>
-
-        <Drawer open={displaySidebar} onClose={closeSidebar} variant="left">
-          <DrawerWrapper onClose={closeSidebar}>
-            <div className="flex flex-col space-y-6 p-5">
-              {siteSettings.sidebarLinks.admin.map(({ href, label, icon }) => (
-                <SidebarItem
-                  href={href}
-                  label={t(label)}
-                  icon={icon}
-                  key={href}
-                />
-              ))}
-            </div>
-          </DrawerWrapper>
-        </Drawer>
-
-        <div className="flex items-center space-s-8">
-          {hasAccess(adminAndOwnerOnly, permissions) && (
-            <LinkButton
-              href={ROUTES.CREATE_SHOP}
-              className="ms-4 md:ms-6"
-              size="small"
-            >
-              {t("common:text-create-shop")}
-            </LinkButton>
-          )}
-
-          <AuthorizedMenu />
-        </div>
-      </nav>
-    </header>
+    <div className="min-h-screen bg-gray-100 flex flex-col transition-colors duration-150">
+      <Navbar />
+      <div className="flex flex-1 pt-20">
+        <aside className="shadow w-72 xl:w-76 hidden lg:block overflow-y-auto bg-white px-4 fixed start-0 bottom-0 h-full pt-22">
+          <div className="flex flex-col space-y-6 py-3">
+            {siteSettings.sidebarLinks.shop.map(
+              ({ href, label, icon, permissions }) => {
+                if (!hasAccess(permissions, currentUserPermissions))
+                  return null;
+                return (
+                  <SidebarItem
+                    key={label}
+                    href={href(shop?.toString()!)}
+                    label={t(label)}
+                    icon={icon}
+                  />
+                );
+              }
+            )}
+          </div>
+        </aside>
+        <main className="w-full lg:ps-72 xl:ps-76">
+          <div className="p-5 md:p-8 overflow-y-auto h-full">{children}</div>
+        </main>
+      </div>
+    </div>
   );
 };
-
-export default Navbar;
+export default ShopLayout;
