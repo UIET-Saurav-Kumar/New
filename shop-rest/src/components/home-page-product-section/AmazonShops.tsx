7200c91fb3330756f6190ec34b97be937b2cbd6e
vinender singh <46550815+vinender@users.noreply.github.com>
2021-10-29 16:56:45 +0530

    Update AmazonShops.tsx

--- shop-rest/src/components/home-page-product-section/AmazonShops.tsx
+++ shop-rest/src/components/home-page-product-section/AmazonShops.tsx
@@ -24,9 +24,9 @@ export default function AmazonShops() {
 
 
     return (  
-        <>
+        <div className='hidden'>
 
-            <div id='ecommerce-store' className='hidden flex flex-col mt-8 border-b rounded-t pb-4 bg-gray-50'>
+            <div id='ecommerce-store' className=' flex flex-col mt-8 border-b rounded-t pb-4 bg-gray-50'>
 
                 <div className='flex justify-between  p-2 px-4'>
                     
@@ -83,7 +83,7 @@ export default function AmazonShops() {
                             ))}
                     </div>
 
-               </>
+               </div>
                    
     )
 }
