35cc8884f38ac108b6c0549d0f6599cca78aa7a7
vinender singh <46550815+vinender@users.noreply.github.com>
2021-11-01 10:50:30 +0530

    Update layout.tsx

--- shop-rest/src/components/layout/layout.tsx
+++ shop-rest/src/components/layout/layout.tsx
@@ -1,11 +1,11 @@
 import Navbar from "@components/layout/navbar/navbar";
 import MobileNavigation from "./mobile-navigation";
 import Footer from "@components/footer/Footer";
-
+import NavbarWithSearch from "./navbar/navbar-with-search";
 
 const Layout: React.FC = ({ children }) => (
   <div className="min-h-screen flex flex-col transition-colors duration-150 bg-gray-100">
-    <Navbar />
+      <NavbarWithSearch />
     <div className="flex-grow">{children}</div>
     <Footer/>
     <MobileNavigation search={false} />
