3bee049b9c751dd08176a20966fc2eec45b4b196
vinender singh <46550815+vinender@users.noreply.github.com>
2021-11-15 17:49:15 +0530

    Update loan-page-header.tsx

--- shop-rest/src/pages/loan-page/loan-page-header.tsx
+++ shop-rest/src/pages/loan-page/loan-page-header.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import Logo from '@components/ui/logo'
 
@@ -5,17 +6,26 @@ export default function LoanPageHeader() {
 
     return (
        <>
-        <div className='px-20 p-6 bg-opacity-60 flex items-center shadow-xl justify-between'>
-            <div className=' flex'>
-                <Logo/>
-            </div>
+            <div style={{ 
+                    // objectFit: 'cover',
+                    // backgroundRepeat: 'no-repeat',
+                    // backgroundColor:'#295939',
+                    // backgroundImage: `url("https://o.remove.bg/downloads/7c9b17c4-7663-4a5c-a90d-340b2f535b4f/loan-background-removebg-preview.png")` 
+                    // backgroundImage: `url("/loan-plant-bg.jpg")`
+                    // https://image.freepik.com/free-vector/bank-loan-isometric-composition-with-icons-contract-credit-card-with-stationery-items-people-illustration_1284-62206.jpg
+                  }}
 
-            <div className='hidden lg:flex items-center space-x-6 justify-evenly'>
-                <h1>Any queries</h1>
-               
-               
+                className='px-20 p-6 h-full bg-transparent flex items-center  justify-between'>
+
+                <div className='flex'>
+                    <Logo/>
+                </div>
+
+                <div className='hidden lg:flex text-white items-center space-x-6 justify-evenly'>
+                    <h1>Any queries?</h1>
+                    {/* <h1>87898-76292</h1> */}
+                </div>
             </div>
-        </div>
        </>
     )
 }
