052fb4bcd967a68f3c445815d02d2ea3ed2c5312
vinender singh <46550815+vinender@users.noreply.github.com>
2021-11-26 12:08:29 +0530

    invoice banner

---
+++ shop-rest/src/components/home-page-product-section/invoice-banner.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Link from 'next/link';
+import { useCategoriesQuery } from "@data/home/use-categories-query";
+import { useRouter } from "next/router";
+import { tail } from 'lodash';
+
+
+export default function InvoiceBanner() {
+
+    const router = useRouter();
+	const { query } = useRouter();
+    const { type } = query;
+
+    function getLink() {
+		var pathname="/"+router.locale+"/user/upload-invoice";
+		// const { type, ...rest } = query;
+		// var text=(query.text)?query.text:"";
+
+		return pathname;
+		// +"?text="+text;
+	}
+
+
+    return (
+
+     <Link href={getLink()}>   
+   <div className='flex w-full mt-8 md:mt-10 h-auto border'>   
+   <img src='/invoice.jpg' className=' md:h-auto object-contain pt-0 lg:w-full -sm-pt-60 lg:object-contain lg:mt-8 xl:object-cover'/>
+            
+              </div> 
+        </Link>
+    )
+}
