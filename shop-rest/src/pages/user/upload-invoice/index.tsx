2d962e20146d1f53a75b251a286ba307c4f2bbbf
vinender singh <46550815+vinender@users.noreply.github.com>
2021-11-26 14:36:22 +0530

    Update index.tsx

--- shop-rest/src/pages/user/upload-invoice/index.tsx
+++ shop-rest/src/pages/user/upload-invoice/index.tsx
@@ -4,21 +4,38 @@ import ErrorMessage from "@components/ui/error-message";
 import Loader from "@components/ui/loader/loader";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import InvoiceUploadList from "@components/bill/invoice-upload-list";
+import InvoiceUploadList from "./invoice-upload-list";
 import LinkButton from "@components/ui/link-button";
-import { useInvoiceUploadQuery} from "@data/bill-upload/use-invoices-upload.query";
+import { useInvoiceUploadQuery} from "./use-invoices-upload.query";
 import {  useState } from "react";
 import { SortOrder } from "@ts-types/generated";
 import ProfileSidebar from "@components/profile/profile-sidebar";
 import SortForm from "@components/common/sort-form";
 import Navbar from '@components/layout/navbar/navbar';
+import UploadBill from "./upload-form";
+import { useModalAction } from "@components/ui/modal/modal.context";
+import { useCustomerQuery } from "@data/customer/use-customer.query";
+import { useEffect } from "react";
+import { useUI } from "@contexts/ui.context";
+
 
 export default function UploadInvoice() {
   const { t } = useTranslation();
   const [page, setPage] = useState(1);
   const [orderBy, setOrder] = useState("created_at");
   const [sortedBy, setColumn] = useState<SortOrder>(SortOrder.Desc);
-
+  const { refetch } = useCustomerQuery();
+  const { isAuthorize } = useUI();
+  const { openModal } = useModalAction();
+  
+  useEffect(() => {
+    if (!isAuthorize) {
+      return openModal("LOGIN_VIEW");
+    }
+    if (isAuthorize) {
+      refetch();
+    }
+  }, [isAuthorize]);
 
   const {
     data,
@@ -51,13 +68,14 @@ export default function UploadInvoice() {
         <div className="w-full overflow-hidden">
           <div className="flex flex-col  justify-evenly p-4 w-full mx-auto  mt-14">
             <Card className="flex flex-col md:flex-row items-center justify-between mb-8">
+              
               <div className="md:w-1/4 mb-4 md:mb-0">
-                <h1 className="text-lg font-semibold text-heading">
-                  {("invoice upload")}
-                </h1>
+                  <h1 className="text-lg font-semibold text-heading">
+                    {("invoice_upload")}
+                  </h1>
               </div>
 
-              <div className="grid grid-cols-1 space-y-4 sm:flex sm:items-center md:flex 
+              {/* <div className="grid grid-cols-1 space-y-4 sm:flex sm:items-center md:flex 
                               md:items-center place-items-center w-full ms-auto">
                     <SortForm
                     showLabel={false}
@@ -76,15 +94,17 @@ export default function UploadInvoice() {
                     />
 
                     <LinkButton
-                    href={`/user/upload-invoice/upload-form`}
+                    href={`/user/invoice-upload/create`}
                     className="h-12 ms-4 md:ms-6"
                     >
                     <span className="hidden md:block">
-                        + {("Upload Invoice")}
+                        + {("Request Withdraw")}
                     </span>
                     <span className="md:hidden">+ {("Add")}</span>
                     </LinkButton>
-              </div>
+              </div> */}
+
+              <UploadBill/>
 
             </Card>
 
