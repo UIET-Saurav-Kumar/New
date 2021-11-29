714bbba5afb46c673d9f8489294154887b23bafb
vinender <vinendersingh91@gmail.com>
2021-11-29 15:00:09 +0530

    shop card

--- shop-rest/src/pages/shops/offer-cards.tsx
+++ shop-rest/src/pages/shops/offer-cards.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import img from 'next/img';
+import Link from 'next/link';
+
 
 export default function OfferCards() {
 
@@ -7,10 +9,10 @@ export default function OfferCards() {
 
         <div className='grid grid-cols-2 gap-2 mt-4 sm:grid-cols-4 w-full 
                     place-content-center '>
-            <div className=''><img layout='fill' objectFit='cover' src='/shop-offer4.jpg' className=' object-cover '/></div>
-            <div className=''><img layout='fill' objectFit='cover' src='/shop-offer3.jpg' className=' object-cover '/></div>
-            <div className=''><img layout='fill' objectFit='cover' src='/shop-offer2.jpg' className=' object-cover '/></div>
-            <div className=''><img layout='fill' objectFit='cover' src='/shop-offer1.jpg' className=' object-cover '/></div>
+            <Link href='/user/upload-invoice/upload-form'><div className=''><img   src='/shop-offer4.jpg' className='cursor-pointer border object-cover '/></div></Link>
+            <Link href='/user/upload-invoice/upload-form'><div className=''><img   src='/shop-offer3.jpg' className=' cursor-pointer border object-cover '/></div></Link>
+            <Link href='/user/invite'><div className=''><img   src='/shop-offer2.jpg' className='cursor-pointer border object-cover '/></div></Link>
+            <Link href='/user/invite'><div className=''><img   src='/shop-offer1.jpg' className='cursor-pointer border-gray-100 hover-border-red-900 object-cover '/></div></Link>
         </div>
     )
 }
