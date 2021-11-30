f3c0419d548e5e4676cc28da9c9d447f2897e0db
vinender singh <46550815+vinender@users.noreply.github.com>
2021-11-30 11:55:05 +0530

    Update tandoor.tsx

--- shop-rest/src/components/home-page-product-section/tandoor.tsx
+++ shop-rest/src/components/home-page-product-section/tandoor.tsx
@@ -7,12 +7,13 @@ import { useLocation } from "@contexts/location/location.context";
 export default function Tandoor() {
 
     const router = useRouter();
-	const { query } = useRouter();
+	  const { query } = useRouter();
     const { type } = query;
     const {getLocation} =useLocation()
 
 
     function getLink(tandoor :any) {
+
 		var pathname="/"+router.locale+"/tandoor-page?category="+tandoor.replace("&","-");
 		// const { type, ...rest } = query;
 		// var text=(query.text)?query.text:"";
@@ -21,16 +22,16 @@ export default function Tandoor() {
 		// +"?text="+text;
 	}
 
-    const address = getLocation?.formattedAddress;
+    const address =   getLocation?.formattedAddress || "chandigarh";
 
     function location(){
-      return  address.includes('Mohali') || address.includes('Chandigarh') || address.includes('Panchkula')
+        return address?.includes('Mohali') || address?.includes('Chandigarh') || address.includes('Panchkula') ;
     }
 
 
     return (
 
-    <div className={` ${location() ? 'block' : 'hidden'}`}><Link className="categories-link"  href={getLink('Tandoors of Chandigarh')}>   
+    <div className={` ${location() ? 'block' : 'hidden'}`}><Link href={getLink('Tandoors of Chandigarh')}>   
    <div className='flex w-full mt-0 -sm-mt-60 border'>   
    <img src='/tandoor-banner.jpg' className=' md:h-70 object-contain pt-0 lg:w-full -sm-pt-60 lg:object-contain lg:mt-8 xl:object-cover'/>
             
