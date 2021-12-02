ec0b28a815b6b9aeb929097438f816628d603191
vinender <vinendersingh91@gmail.com>
2021-12-02 17:03:25 +0530

    invoice upload auth

--- shop-rest/src/components/home-page-product-section/invoice-banner.tsx
+++ shop-rest/src/components/home-page-product-section/invoice-banner.tsx
@@ -2,32 +2,41 @@ import React from 'react'
 import Link from 'next/link';
 import { useCategoriesQuery } from "@data/home/use-categories-query";
 import { useRouter } from "next/router";
-import { tail } from 'lodash';
+import { useUI } from "@contexts/ui.context";
+import { useModalAction } from "@components/ui/modal/modal.context";
 
 
 export default function InvoiceBanner() {
 
     const router = useRouter();
 	const { query } = useRouter();
+    const { isAuthorize } = useUI();
+    const { openModal } = useModalAction();
+
+
     const { type } = query;
 
     function getLink() {
-		var pathname="/user/upload-invoice";
-		// const { type, ...rest } = query;
-		// var text=(query.text)?query.text:"";
-
-		return pathname;
+		
+        if (isAuthorize) {
+            router.push("/user/upload-invoice")
+          
+          }
+          if (!isAuthorize) {
+            
+            return openModal("LOGIN_VIEW");
+          }
 		// +"?text="+text;
 	}
 
 
     return (
 
-     <Link href={getLink()}>   
-   <div className='flex w-full mt-8 md:mt-10 h-auto '>   
+   
+   <div onClick={getLink} className='flex w-full mt-8 md:mt-10 h-auto '>   
    <img src='/invoice.jpg' className=' md:h-auto object-contain cursor-pointer pt-0 lg:w-full -sm-pt-60 lg:object-contain lg:mt-8 xl:object-cover'/>
             
               </div> 
-        </Link>
+       
     )
 }
