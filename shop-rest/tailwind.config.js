239d53b8b40e8f789e2e5d8d139661de8518918e
vinender <vinendersingh91@gmail.com>
2021-12-02 13:16:05 +0530

    footer

--- shop-rest/tailwind.config.js
+++ shop-rest/tailwind.config.js
@@ -192,6 +192,25 @@ module.exports = {
       borderRadius: {
         DEFAULT: "5px",
       },
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+
+       '3': '3px',
+        '4': '4px',
+
+       '6': '6px',
+
+       '8': '8px',
+       '10': '10px',
+       '12': '12px',
+        '14': '14px',
+        '16': '16px',
+        '18': '18px',
+        '20': '20px',
+
+      },
       inset: {
         22: "5.25rem",
       },
