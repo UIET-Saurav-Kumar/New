d544ff836f136d3b026e4569650646962da43a97
vinender singh <46550815+vinender@users.noreply.github.com>
2021-12-02 11:05:38 +0530

    Update Footer.tsx

--- shop-rest/src/components/footer/Footer.tsx
+++ shop-rest/src/components/footer/Footer.tsx
@@ -16,32 +16,41 @@ const Footer = () => {
   }
 
 	return (
-		<div className='footer-wrapper 
-		text-10px py-2
-	   bg-black shadow-lg mb-8 rounded-md text-white lg:px-20 justify-evenly mt-16'>
+
+		<>
+		{/* <img src='/pink-footer.jpg' className =' relative bg-fixed z-10 w-full object-cover' /> */}
+		<div style ={{
+            // backgroundImage: `url(${'/pink-footer.jpg'})`,
+			// backgroundRepeat: 'no-repeat',
+			// backgroundSize: '1900px 900px',
+			// height: '900px',
+			// objectFit: 'fill',
+		}} className=' bg-pink-700 sm:bg-pink-footer h-full bg-cover bg-no-repeat footer-wrapper 
+		      text-10px py-2 pt-10
+	          mb-0 rounded-md text-white  lg:px-20 justify-evenly mt-16'>
 			  
 
 			  <div className='footer-top-section 
-                             grid  grid-cols-1  space-y-10 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4
-							   space-x-6 text-left mb-8 p-4  shadow-md border-gray-700 justify-evenly'>
+                              grid  grid-cols-1  space-y-10 sm:grid-cols-2 md:grid-cols-3  lg:grid-cols-4
+							  space-x-6 text-left mb-8 p-4   border-gray-700 justify-evenly'>
 
 
 			  	<div className='footer-section-links ml-6 lg:ml-0 space-y-4 mt- text-left'>
 
 
-			  		<h5 className='footer-links-title mt-10 font-semibold text-xl tracking-widest'> About  </h5>
+			  		<h5 className='footer-links-title mt-10 font-bold text-xl tracking-widest'> About  </h5>
 
 					  <div className=' tracking-widest   space-y-4'>
 
-						<Link href='./about-us'><h5 className='phone-number cursor-pointer text-lg hover:underline text-primary font-normal -mt-'> About Buylowcal</h5></Link>
-						<Link href='./careers'><h5 className='phone-number cursor-pointer text-lg hover:underline text-primary font-normal -mt-'> Careers</h5></Link>
-						<Link href='./terms'><h5 className='phone-number cursor-pointer text-lg hover:underline text-primary font-normal -mt-'> Terms of Service</h5></Link>
+						<Link href='./about-us'><h5 className='phone-number cursor-pointer text-xl hover:underline text-primary font-normal -mt-'> About Buylowcal</h5></Link>
+						<Link href='./careers'><h5 className='phone-number cursor-pointer text-xl hover:underline text-primary font-normal -mt-'> Careers</h5></Link>
+						<Link href='./terms'><h5 className='phone-number cursor-pointer text-xl hover:underline text-primary font-normal -mt-'> Terms of Service</h5></Link>
 
 
 
-						<Link href='./return-policy'><h5 className=' cursor-pointer text-lg hover:underline phone-number text-primary font-normal -mt-'> Refunds/Cancellations/Shipping</h5></Link> 
-						<Link href='./privacy'><h5 className=' cursor-pointer text-lg hover:underline phone-number text-primary font-normal -mt-'>Privacy Policy</h5></Link> 
-						<Link href='./contact'><h5 className=' cursor-pointer text-lg hover:underline phone-number text-primary font-normal -mt-'>Contact Us</h5></Link> 
+						<Link href='./return-policy'><h5 className=' cursor-pointer text-xl hover:underline phone-number text-primary font-normal -mt-'> Refunds/Cancellations/Shipping</h5></Link> 
+						<Link href='./privacy'><h5 className=' cursor-pointer text-xl hover:underline phone-number text-primary font-normal -mt-'>Privacy Policy</h5></Link> 
+						<Link href='./contact'><h5 className=' cursor-pointer text-xl hover:underline phone-number text-primary font-normal -mt-'>Contact Us</h5></Link> 
 
 					 </div>
 			  		
@@ -51,25 +60,25 @@ const Footer = () => {
 
 				  <div className='footer-section-links  space-y-4 text-left'>
 
-			  		<h5 className = 'footer-links-title font-semibold tracking-widest  text-xl' > More </h5>
+			  		<h5 className = 'footer-links-title font-bold tracking-widest  text-xl' > More </h5>
 
 					<div className=' space-y-0 tracking-widest'> 
-					  <a className='light-text cursor-pointer text-lg hover:underline font-light mt-' href='./home#all-categories'><h5 className='light-text mt-4 font-light'>Categories</h5></a>
-					  <a className='light-text cursor-pointer text-lg hover:underline font-light mt-' href='./home#featured-shops'><h5 className='light-text mt-4 font-light'> Local Shops</h5></a>
-			  	      <a className='light-text cursor-pointer text-lg hover:underline font-light mt-' href='./home#ecommerce-store'><h5 className='light-text mt-4 font-light'> E-Stores</h5></a>	
-					  <a className='light-text cursor-pointer text-lg hover:underline font-light mt-' href='./home#offer-of-the-day'><h5  className='light-text mt-4 font-light'> Offer of the Day</h5></a>
-					  <a className='light-text cursor-pointer text-lg hover:underline font-light ' href='./home#featured-products'><h5  className='light-text mt-4 font-light'> Featured Products</h5></a>
+					  <a className='light-text cursor-pointer text-xl hover:underline font-light mt-' href='./home#all-categories'><h5 className='light-text mt-4 font-light'>Categories</h5></a>
+					  <a className='light-text cursor-pointer text-xl hover:underline font-light mt-' href='./home#featured-shops'><h5 className='light-text mt-4 font-light'> Local Shops</h5></a>
+			  	      <a className='light-text cursor-pointer text-xl hover:underline font-light mt-' href='./home#ecommerce-store'><h5 className='light-text mt-4 font-light'> E-Stores</h5></a>	
+					  <a className='light-text cursor-pointer text-xl hover:underline font-light mt-' href='./home#offer-of-the-day'><h5  className='light-text mt-4 font-light'> Offer of the Day</h5></a>
+					  <a className='light-text cursor-pointer text-xl hover:underline font-light ' href='./home#featured-products'><h5  className='light-text mt-4 font-light'> Featured Products</h5></a>
 			  		</div> 
 			  	</div>
 
 				  <div className='footer-section-links  space-y-4 text-left'>
-						<h5 className='footer-links-title font-semibold text-xl tracking-widest '>Registrations</h5>
+						<h5 className='footer-links-title font-bold text-xl tracking-widest '>Registrations</h5>
 
 						<div className=' space-y-4 tracking-widest '>
 							<Link href='https://admin.buylowcal.com/register'>
-								<h5 className='light-text cursor-pointer text-lg hover:underline  font-light'>Register as Vendor </h5>
+								<h5 className='light-text cursor-pointer text-xl hover:underline  font-light'>Register as Vendor </h5>
 							</Link>
-							<h5 onClick={handleJoin} className='light-text cursor-pointer text-lg hover:underline font-light'>
+							<h5 onClick={handleJoin} className='light-text cursor-pointer text-xl hover:underline font-light'>
 								Register as Customer 
 							</h5>
 						</div>		  		
@@ -77,39 +86,34 @@ const Footer = () => {
 
 
 
-				  <div className='flex flex-col  space-y-4'>
-					  <h5 className='font-semibold text-xl tracking-widest'> Follow Us</h5>
-				  <div className='grid grid-cols-2 xs++:grid-cols-4 md:grid-cols-2 lg:grid-cols-4 place-items-center items-center gap-8  w-60 md:w-72 cursor-pointer text-3xl lg:text-lg '>
-					  
-					<a target="_blank" href='https://www.facebook.com/buylowcal/'> <FacebookIcon className='h-8 w-8  bg-blue-800 cursor-pointer text-lg hover:underline'/></a> 
-					  
-					<a target="_blank" href='https://www.instagram.com/buylowcal/?hl=en'> <InstagramIcon className='h-8 w-8 text-red-500 cursor-pointer text-lg hover:underline'  /></a>
+					<div className='flex flex-col  space-y-4'>
+						<h5 id='heading' className='font-bold w-full text-xl md:text-xl lg:text-xl tracking-widest'> Follow Us</h5>
+						<div className='grid grid-cols-4 xs++:grid-cols-4 md:grid-cols-2 lg:flex lg:justify-between w-full place-items-center items-center  lg:gap-0  md:w-56 cursor-pointer text-3xl lg:text-xl'>
+							
+							<a target="_blank" href='https://www.facebook.com/buylowcal/'><div className='relative z-10 w-10 h-10 sm:w-16 sm:h-16 bg- rounded-full'> <img src='/fb-white.png' className='absolute z-100 h-10 w-10 hover:bg-blue-600 rounded-full   cursor-pointer text-xl hover:underline'/></div></a> 
+							
+							<a target="_blank" href='https://www.instagram.com/buylowcal/?hl=en'><div className='relative z-10 w-10 h-10 sm:w-16 sm:h-16 bg- rounded-full'> <img src='/insta-white.png' className='absolute z-100 h-10 w-10 hover:bg-red-500 rounded-full  text-red-500 cursor-pointer text-xl hover:underline'/></div></a>
 
-					<a target="_blank" href='https://in.linkedin.com/company/buylowcal'> <LinkedInIcon className='h-8 w-8 cursor-pointer hover:underline '  />	 </a>			
+							<a target="_blank" href='https://in.linkedin.com/company/buylowcal'><div className='relative z-10 w-10 h-10 sm:w-16 sm:h-16 bg- rounded-full'> <img src='/linked-in.png' className='absolute z-100 h-10 w-10 hover:bg-blue-600  rounded-full  cursor-pointer hover:underline'/></div></a>			
 
-					<a target="_blank" href='https://www.youtube.com/channel/UCs0jYR_99h5GKs0cvRZSg-A'> <YouTubeIcon className='h-8 w-8 cursor-pointer hover:underline '  />	 </a>		
+							<a target="_blank" href='https://www.youtube.com/channel/UCs0jYR_99h5GKs0cvRZSg-A'><div className='relative z-10 w-10 h-10 sm:w-16 sm:h-16 bg- rounded-full'> <img src='/youtube-white.png' className='absolute z-100 h-10 w-10 hover:bg-red-600 rounded-full  cursor-pointer hover:underline '/></div></a>		
 
-				 </div>
-				  </div>
+						</div>
+					</div>
 
 
 			  </div>
 			  
-			  <div className='footer-bottom-section flex justify-between items-start p-2 px-4 -mt-0'>
+			  <div className='footer-bottom-section text-center sm:text-right w-full mb-16 sm:mb-6 p-2 px-4 -mt-0'>
 			  	 
-				    <h5 className='  font-semibold text-lg text-gray-400'> 
+				    <h5 id='heading' className='font-semibold text-sm sm:text-xl text-gray-200'> 
 					 © 4221 BuyLowcal.com All Rights Reserved
 				    </h5>
-			  	  <div className='social-media-icons-wrapper flex justify-between w-40'>
-			  	  	{/* <FacebookIcon className='facebook'/>
-			  	  	<TwittterIcon className='twitter'/>
-			  	  	<InstagramIcon className='instagram'/>
-			  	  	<YouTubeIcon className='youtube'/> */}
-
-			  	  </div>
+ 
 			  </div>
 
 		</div>
+		</>
 	)
 }
 
