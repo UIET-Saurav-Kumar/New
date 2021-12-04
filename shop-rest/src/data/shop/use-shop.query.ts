1b24184bdcca2fca3b3f09797eda1dbcdf91465e
vinender singh <46550815+vinender@users.noreply.github.com>
2021-12-04 16:01:58 +0530

    Update use-shop.query.ts

--- shop-rest/src/data/shop/use-shop.query.ts
+++ shop-rest/src/data/shop/use-shop.query.ts
@@ -4,13 +4,12 @@ import http from "@utils/api/http";
 import url from "@utils/api/server_url";
 import { useQuery } from "react-query";
 
-export const fetchShop = async (slug: string) => {
-  const { data } = await http.get(`${url}/${API_ENDPOINTS.SHOPS}/${slug}`);
+export const fetchShopSeo = async (slug: string) => {
+  const { data } = await http.get(`${url}/${API_ENDPOINTS.SETTINGS}?shop_slug=${slug}`);
   return data;
 };
-
 export const useShopQuery = (slug: string) => {
   return useQuery<Shop, Error>([API_ENDPOINTS.SHOPS, slug], () =>
-    fetchShop(slug)
+    fetchShopSeo(slug)
   );
 };
