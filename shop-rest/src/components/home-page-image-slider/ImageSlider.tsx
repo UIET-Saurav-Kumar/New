b214b8e99416167b04c612ba4c84f7de66a47776
vinender <vinendersingh91@gmail.com>
2021-12-14 15:06:12 +0530

    banner

--- shop-rest/src/components/home-page-image-slider/ImageSlider.tsx
+++ shop-rest/src/components/home-page-image-slider/ImageSlider.tsx
@@ -55,6 +55,15 @@ export default function ImageSlider() {
             </div>
         </div>
 
+        <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
+
+            <Image  layout='fill' objectFit='fill'
+            //  className='rounded-md w-full opacity-90  h-40 lg:w-full lg:h-72 xl+:h-80 md:h-64  2xl:h-72 2xl:w-full space-x-9 object-fill object '  
+                  src={'/banner/grocery-offer.jpg'} 
+                  />
+
+          </div>
+
 
          <div>
             <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
@@ -70,30 +79,23 @@ export default function ImageSlider() {
 
       
 
-          <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
-
-            <Image  layout='fill' objectFit='fill'
-            //  className='rounded-md w-full opacity-90  h-40 lg:w-full lg:h-72 xl+:h-80 md:h-64  2xl:h-72 2xl:w-full space-x-9 object-fill object '  
-                  src={'/banner/grocery-offer.jpg'} 
-                  />
-
-          </div>
+          
 
         
-          <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
+          {/* <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
             <Image  layout='fill' objectFit='fill'
             //  className='rounded-md  w-full h-40 lg:w-full lg:h-72 xl+:h-80 md:h-64  2xl:h-72 2xl:w-full object-fill  space-x-9 '   
                   src={'/banner/salon.jpg'} 
                   />
-          </div> 
+          </div>  */}
     
 
-          <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
+          {/* <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
             <Image layout='fill' objectFit='fill'
             //  className='rounded-md w-full h-40 lg:w-full lg:h-72 2xl:h-72 xl+:h-80 md:h-64 2xl:w-full object-fill  space-x-9 '   
                 src={'/banner/pharma.jpg'} 
                 />
-          </div>
+          </div> */}
         
 
         
@@ -108,28 +110,28 @@ export default function ImageSlider() {
       
 
         
-          <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
+          {/* <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
 
             <Image layout='fill' objectFit='fill'
             //  className='rounded-md  w-full  h-40 lg:w-full lg:h-72 xl+:h-80 md:h-64 2xl:h-72 2xl:w-full object-fill space-x-9 '   
                 src={'/banner/gym.jpg'} 
                 />
 
-          </div>
+          </div> */}
         
 
         
-          <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
+          {/* <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
 
             <Image layout='fill' objectFit='fill'
             //  className='rounded-md  w-full  h-40 lg:w-full xl+:h-80 md:h-64 lg:h-72 2xl:h-72 2xl:w-full object-fill  space-x-9 '   
                 src={'/banner/furniture.jpg'} 
                 />
 
-          </div>
+          </div> */}
       
 
-        <div>
+        {/* <div>
           <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
 
             <Image layout='fill' objectFit='fill'
@@ -138,7 +140,7 @@ export default function ImageSlider() {
             />
 
           </div>
-        </div>
+        </div> */}
 
 
        
