d0d39a6cfe84ebfa60d249ba7d7c9a5d9692358c
vinender singh <46550815+vinender@users.noreply.github.com>
2021-12-16 12:00:58 +0530

    Update ImageSlider.tsx

--- shop-rest/src/components/home-page-image-slider/ImageSlider.tsx
+++ shop-rest/src/components/home-page-image-slider/ImageSlider.tsx
@@ -65,16 +65,7 @@ export default function ImageSlider() {
         </div>
 
 
-         <div>
-            <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
-
-                <Image layout='fill' objectFit='fill'
-                //  className='rounded-md  w-full  h-40 lg:w-full xl+:h-80 md:h-64 lg:h-72  2xl:h-full 2xl:w-full object-cover object  space-x-9 '   
-                    src={'/banner/restaurant.jpg'} 
-                    />
-
-            </div>
-        </div>
+       
 
         
           {/* <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
