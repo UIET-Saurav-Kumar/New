b1701ce8513e53071ae5cf7ff82f10c5fc7a116a
vinender <vinendersingh91@gmail.com>
2022-01-08 10:28:51 +0530

    seo

--- shop-rest/src/pages/_app.tsx
+++ shop-rest/src/pages/_app.tsx
@@ -1,35 +1,3 @@
-// import type { AppProps /*, AppContext */ } from "next/app";
-// import "@fontsource/open-sans";
-// import "@fontsource/open-sans/600.css";
-// import "@fontsource/open-sans/700.css";
-// import "@assets/slick.css";
-// import "@assets/slick-theme.css";
-// import "@assets/main.css";
-// import "react-toastify/dist/ReactToastify.css";
-// import { UIProvider, useUI } from "@contexts/ui.context";
-// import { SearchProvider } from "@contexts/search.context";
-// import { CheckoutProvider } from "@contexts/checkout.context";
-// import SidebarContainer from "@components/common/sidebar/sidebar-container";
-// import ErrorMessage from "@components/ui/error-message";
-// import { SettingsProvider } from "@contexts/settings.context";
-// import PageLoader from "@components/ui/page-loader/page-loader";
-// import { useSettingsQuery } from "@data/settings/use-settings.query";
-// import { QueryClient, QueryClientProvider } from "react-query";
-// import { Hydrate } from "react-query/hydration";
-// import { ReactQueryDevtools } from "react-query/devtools";
-// import { appWithTranslation } from "next-i18next";
-// import { useEffect, useRef, useState } from "react";
-// import { ToastContainer } from "react-toastify";
-// import { CartProvider } from "@contexts/quick-cart/cart.context";
-// import { LocationProvider } from "@contexts/location/location.context";
-// import Seo from "@components/ui/seo";
-// import { useSession } from "next-auth/client";
-// import { useSocialLoginMutation } from "@data/auth/use-social-login-mutation";
-// import { CUSTOMER } from "@utils/constants";
-// import Cookies from "js-cookie";
-// import { fetchShop } from "@data/shop/use-shop.query";
-// import ManagedModal from "@components/ui/modal/managed-modal";
-
 import type { AppProps /*, AppContext */ } from "next/app";
 import "@fontsource/open-sans";
 import "@fontsource/open-sans/600.css";
@@ -59,19 +27,39 @@ import { useSession } from "next-auth/client";
 import { useSocialLoginMutation } from "@data/auth/use-social-login-mutation";
 import { CUSTOMER } from "@utils/constants";
 import Cookies from "js-cookie";
+import { fetchShop } from "@data/shop/use-shop.query";
 import ManagedModal from "@components/ui/modal/managed-modal";
+import { fetchShopSeo } from "@data/shop/use-shop.query";
 import {
   ModalProvider,
   useModalAction,
 } from "@components/ui/modal/modal.context";
 
+
 const Noop: React.FC = ({ children }) => <>{children}</>;
 
 const AppSettings: React.FC = (props) => {
-  const { data, isLoading: loading, error } = useSettingsQuery();
-  if (loading) return <PageLoader />;
-  if (error) return <ErrorMessage message={error.message} />;
-  return <SettingsProvider initialValue={data?.settings?.options} {...props} />;
+  const routname  = props?.children?._owner?.pendingProps?.router?.route.split('/')[1];
+  const slugname  = props?.children?._owner?.pendingProps?.router?.query?.slug;
+  if(slugname !== undefined && routname == 'shops')
+  {
+    const { data, isLoading: loading, error } = useSettingsQuery();
+
+    const shop = fetchShopSeo(props?.children?._owner?.pendingProps?.router?.query?.slug as string);
+    console.log('app tsx console');
+    console.log(shop);
+    if (loading) return <PageLoader />;
+    if (error) return <ErrorMessage message={error.message} />;
+    return <SettingsProvider initialValue={shop?.settings?.options} {...props} />;
+  }
+  else
+  {
+    const { data, isLoading: loading, error } = useSettingsQuery();
+
+    if (loading) return <PageLoader />;
+    if (error) return <ErrorMessage message={error.message} />;
+    return <SettingsProvider initialValue={data?.settings?.options} {...props} />;
+  }
 };
 
 const SocialLoginProvider: React.FC = () => {
@@ -157,133 +145,3 @@ function CustomApp({ Component, pageProps }: AppProps) {
 }
 
 export default appWithTranslation(CustomApp);
-
-// import { fetchShopSeo } from "@data/shop/use-shop.query";
-// import {
-//   ModalProvider,
-//   useModalAction,
-// } from "@components/ui/modal/modal.context";
-// import { test } from "@data/shop/use-shop.query";
-
-// const Noop: React.FC = ({ children }) => <>{children}</>;
-
-// const AppSettings: React.FC = (props) => {
-//   // console.log('%c inside appsetting apptsx test','color:red', test);
-//   // const testfunction = test();
-//   // console.log('%c inside appsetting testfunciton','color:red', testfunction);
-//   const routname  = props?.children?._owner?.pendingProps?.router?.route.split('/')[1];
-//   const slugname  = props?.children?._owner?.pendingProps?.router?.query?.slug;
-//   // console.log('routname',routname);
-//   // console.log('slugname', slugname);
-//   if(slugname !== undefined && routname == 'shops')
- 
-//   {
-//     // console.log('slugname', slugname);
-//     const { data, isLoading: loading, error } = useSettingsQuery();
-
-//     const shop = fetchShopSeo(props?.children?._owner?.pendingProps?.router?.query?.slug as string);
-//     console.log('app tsx console');
-//   //   console.log('%c inside fetchshopSEO test','color:red', test);
-//   // const testfunction = test();
-//   // console.log('%c inside fetchshopSEO  testfunciton','color:red', testfunction);
-//     console.log('shop',shop);
-//     if (loading) return <PageLoader />;
-//     if (error) return <ErrorMessage message={error.message} />;
-//     return <SettingsProvider initialValue={shop?.settings?.options} {...props} />;
-//   }
-//   else
-//   {
-//     const { data, isLoading: loading, error } = useSettingsQuery();
-//     console.log('else app tsx console');
-
-//     if (loading) return <PageLoader />;
-//     if (error) return <ErrorMessage message={error.message} />;
-//     return <SettingsProvider initialValue={data?.settings?.options} {...props} />;
-//   }
-// };
-
-
-
-// const SocialLoginProvider: React.FC = () => {
-//   const [session, loading] = useSession();
-//   const { mutate: socialLogin } = useSocialLoginMutation();
-//   const { closeModal } = useModalAction();
-//   const { authorize, isAuthorize } = useUI();
-//   const [errorMsg, setErrorMsg] = useState("");
-
-//   useEffect(() => {
-//     // is true when valid social login access token and provider is available in the session
-//     // but not authorize/logged in yet
-//     if (!isAuthorize && session?.accessToken && session?.provider) {
-//       socialLogin(
-//         {
-//           provider: session?.provider as string,
-//           access_token: session?.accessToken as string,
-//         },
-//         {
-//           onSuccess: (data) => {
-//             if (data?.token && data?.permissions?.includes(CUSTOMER)) {
-//               Cookies.set("auth_token", data.token);
-//               Cookies.set("auth_permissions", data.permissions);
-//               authorize();
-//               closeModal();
-//             }
-//             if (!data.token) {
-//               setErrorMsg("The credentials was wrong!");
-//             }
-//             if (!data.permissions.includes(CUSTOMER)) {
-//               setErrorMsg("Doesn't have enough permission");
-//             }
-//           },
-//           onError: (error: any) => {
-//             console.log(error.message);
-//           },
-//         }
-//       );
-//     }
-//   }, [isAuthorize, session]);
-
-//   // When rendering client side don't display anything until loading is complete
-//   if (typeof window !== "undefined" && loading) return null;
-
-//   return <div>{errorMsg}</div>;
-// };
-
-// function CustomApp({ Component, pageProps }: AppProps) {
-//   const queryClientRef = useRef<any>(null);
-//   if (!queryClientRef.current) {
-//     queryClientRef.current = new QueryClient();
-//   }
-//   const Layout = (Component as any).Layout || Noop;
-//   return (
-//     <QueryClientProvider client={queryClientRef.current}>
-//       <Hydrate state={pageProps.dehydratedState}>
-//         <AppSettings>
-//           <ModalProvider>
-//             <CartProvider>
-//               <LocationProvider>
-//                 <UIProvider>
-//                   <CheckoutProvider>
-//                     <SearchProvider>
-//                       <Layout {...pageProps}>
-//                         <Seo />
-//                         <Component {...pageProps} />
-//                       </Layout>
-//                       <ToastContainer autoClose={2000} />
-//                       <ManagedModal />
-//                       <SidebarContainer />
-//                     </SearchProvider>
-//                   </CheckoutProvider>
-//                   <SocialLoginProvider />
-//                 </UIProvider>
-//               </LocationProvider>
-//             </CartProvider>
-//           </ModalProvider>
-//         </AppSettings>
-//         {/* <ReactQueryDevtools /> */}
-//       </Hydrate>
-//     </QueryClientProvider>
-//   );
-// }
-
-// export default appWithTranslation(CustomApp);
