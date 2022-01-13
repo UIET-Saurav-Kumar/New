b4aa078fcd841b4f534e97e6c1f487a3ebb13986
vinender singh <46550815+vinender@users.noreply.github.com>
2022-01-13 13:49:22 +0530

    Update managed-modal.tsx

--- admin-rest/src/components/ui/modal/managed-modal.tsx
+++ admin-rest/src/components/ui/modal/managed-modal.tsx
@@ -59,6 +59,9 @@ const MasterProductsExportImport = dynamic(
 const ShopsExportImport = dynamic(
   () => import("@components/product/import-export-modal-shop")
 );
+const OrdersExportImport = dynamic(
+  () => import("@components/product/import-export-modal-orders")
+);
 const LogDeleteView = dynamic(
   () => import("@components/logs/log-delete-view")
 );
@@ -90,6 +93,7 @@ const ManagedModal = () => {
       {view === "EXPORT_IMPORT_ATTRIBUTE" && <AttributeExportImport />}
       {view === "EXPORT_IMPORT_ADMIN_PRODUCT" && <AllProductsExportImport />}
       {view === "EXPORT_IMPORT_SHOPS" && <ShopsExportImport />}
+      {view === "EXPORT_IMPORT_ORDERS" && <OrdersExportImport />}
       {view === "EXPORT_IMPORT_MASTER_PRODUCT" && <MasterProductsExportImport />}
 
     </Modal>
