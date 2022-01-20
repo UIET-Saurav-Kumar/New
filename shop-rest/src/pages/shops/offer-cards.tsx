d310dda9c650b370ba0da2d8ad87230b81e172dd
vinender singh <46550815+vinender@users.noreply.github.com>
2022-01-20 18:33:48 +0530

    Update offer-cards.tsx

--- shop-rest/src/pages/shops/offer-cards.tsx
+++ shop-rest/src/pages/shops/offer-cards.tsx
@@ -9,10 +9,10 @@ export default function OfferCards() {
 
         <div className='grid grid-cols-2 gap-2 mt-4 sm:grid-cols-4 w-full 
                     place-content-center '>
-            <Link href='/user/upload-invoice/upload-form'><div className=''><img   src='/shop-offer4.jpg' className='cursor-pointer border object-cover '/></div></Link>
-            <Link href='/user/upload-invoice/upload-form'><div className=''><img   src='/shop-offer3.jpg' className=' cursor-pointer border object-cover '/></div></Link>
-            <Link href='/user/invite'><div className=''><img   src='/shop-offer2.jpg' className='cursor-pointer border object-cover '/></div></Link>
-            <Link href='/user/invite'><div className=''><img   src='/shop-offer1.jpg' className='cursor-pointer border-gray-100 hover-border-red-900 object-cover '/></div></Link>
+            <Link target='_blank' href='/user/upload-invoice/upload-form'><div className=''><img   src='/shop-offer4.jpg' className='cursor-pointer border object-cover '/></div></Link>
+            <Link target='_blank' href='/user/upload-invoice/upload-form'><div className=''><img   src='/shop-offer3.jpg' className=' cursor-pointer border object-cover '/></div></Link>
+            <Link target='_blank' href='/user/invite'><div className=''><img   src='/shop-offer2.jpg' className='cursor-pointer border object-cover '/></div></Link>
+            <Link target='_blank' href='/user/invite'><div className=''><img   src='/shop-offer1.jpg' className='cursor-pointer border-gray-100 hover-border-red-900 object-cover '/></div></Link>
         </div>
     )
 }
