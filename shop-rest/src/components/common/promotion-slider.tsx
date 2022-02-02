35d6690e1e8e691a3ec6293c19e6e7f219615dca
vinender singh <46550815+vinender@users.noreply.github.com>
2022-02-02 16:16:46 +0530

    Update promotion-slider.tsx

--- shop-rest/src/components/common/promotion-slider.tsx
+++ shop-rest/src/components/common/promotion-slider.tsx
@@ -5,49 +5,94 @@ import { useTranslation } from "next-i18next";
 import "swiper/swiper-bundle.css";
 // dummy data
 const data = [
+
+  {
+    id: 3,
+    title: "banner:promotion-slide-three",
+    bannerUrl: "/ad-banner/3.jpeg",
+  },
+
+  {
+    id: 8,
+    title: "banner:promotion-slide-eight",
+    bannerUrl: "/ad-banner/8.jpeg",
+  },
+  {
+    id: 9,
+    title: "banner:promotion-slide-nine",
+    bannerUrl: "/ad-banner/9.jpeg",
+  },
+  {
+    id: 10,
+    title: "banner:promotion-slide-ten",
+    bannerUrl: "/ad-banner/10.jpeg",
+  },
+  
+  {
+    id: 11,
+    title: "banner:promotion-slide-eleven",
+    bannerUrl: "/ad-banner/11.jpeg",
+  },
+
   {
     id: 1,
     title: "banner:promotion-slide-one",
-    bannerUrl: "/promotion/offer-1.png",
+    bannerUrl: "/ad-banner/1.jpeg",
   },
   {
     id: 2,
     title: "banner:promotion-slide-two",
-    bannerUrl: "/promotion/offer-2.png",
+    bannerUrl: "/ad-banner/2.jpeg",
   },
-  {
-    id: 3,
-    title: "banner:promotion-slide-three",
-    bannerUrl: "/promotion/offer-3.png",
-  },
+  
   {
     id: 4,
     title: "banner:promotion-slide-four",
-    bannerUrl: "/promotion/offer-4.png",
+    bannerUrl: "/ad-banner/4.jpeg",
   },
   {
     id: 5,
     title: "banner:promotion-slide-five",
-    bannerUrl: "/promotion/offer-5.png",
+    bannerUrl: "/ad-banner/5.jpeg",
   },
+  {
+    id: 6,
+    title: "banner:promotion-slide-six",
+    bannerUrl: "/ad-banner/6.jpeg",
+  },
+  {
+    id: 7,
+    title: "banner:promotion-slide-seven",
+    bannerUrl: "/ad-banner/7.jpeg",
+  },
+ 
+  
+
+ 
+
+
 ];
 
 const offerSliderBreakpoints = {
-  320: {
-    slidesPerView: 1,
-    spaceBetween: 0,
-  },
-  580: {
-    slidesPerView: 2,
-    spaceBetween: 16,
-  },
-  1024: {
+  240: {
     slidesPerView: 3,
     spaceBetween: 16,
   },
+  320: {
+    slidesPerView: 3,
+    spaceBetween: 5,
+  },
+  580: {
+    slidesPerView: 6,
+    spaceBetween: 2,
+  },
+  1024: {
+    slidesPerView: 6,
+    spaceBetween: 10,
+  },
   1920: {
-    slidesPerView: 4,
-    spaceBetween: 24,
+    slidesPerView: 6,
+    spaceBetween: 10,
   },
 };
 SwiperCore.use([Navigation]);
@@ -55,7 +100,7 @@ SwiperCore.use([Navigation]);
 export default function PromotionSlider() {
   const { t } = useTranslation();
   return (
-    <div className="px-6 py-5 md:px-8 xl:px-12 md:py-10 border-t border-border-200">
+    <div className=" px-2 md:px-8 xl:px-8  ">
       <div className="relative">
         <Swiper
           id="offer"
@@ -69,28 +114,27 @@ export default function PromotionSlider() {
           {data?.map((d) => (
             <SwiperSlide key={d.id}>
               <img
-                className="w-full h-auto"
+                className="w-28 object-fit h-28 lg:h-44 lg:w-44 border-t "
                 src={d.bannerUrl}
-                alt={t(d.title)}
-                width="430"
-                height="200"
+                // alt={t(d.title)}
+              
               />
             </SwiperSlide>
           ))}
         </Swiper>
         <div
-          className="prev cursor-pointer absolute top-2/4 -start-4 md:-start-5 z-10 -mt-4 md:-mt-5 w-8 h-8 md:w-9 md:h-9 rounded-full bg-light shadow-xl border border-border-200 border-opacity-70 flex items-center justify-center text-heading transition-all duration-200 hover:bg-accent hover:text-light hover:border-accent"
+          className="prev cursor-pointer absolute top-2/4 -start-2 md:-start-5 z-10 -mt-4 md:-mt-5 w-8 h-8 md:w-9 md:h-9 rounded-full bg-light shadow-xl border border-border-200 border-opacity-70 flex items-center justify-center text-heading transition-all duration-200 hover:bg-accent hover:text-light hover:border-accent"
           role="button"
         >
           <span className="sr-only">{t("common:text-previous")}</span>
-          <ArrowPrev width={18} height={18} />
+          <ArrowPrev width={24} height={24} />
         </div>
         <div
-          className="next cursor-pointer absolute top-2/4 -end-4 md:-end-5 z-10 -mt-4 md:-mt-5 w-8 h-8 md:w-9 md:h-9 rounded-full bg-light shadow-xl border border-border-200 border-opacity-70 flex items-center justify-center text-heading transition-all duration-200 hover:bg-accent hover:text-light hover:border-accent"
+          className="next cursor-pointer absolute top-2/4 -end-2 md:-end-5 z-10 -mt-4 md:-mt-5 w-8 h-8 md:w-9 md:h-9 rounded-full bg-light shadow-xl border border-border-200 border-opacity-70 flex items-center justify-center text-heading transition-all duration-200 hover:bg-accent hover:text-light hover:border-accent"
           role="button"
         >
           <span className="sr-only">{t("common:text-next")}</span>
-          <ArrowNext width={18} height={18} />
+          <ArrowNext width={24} height={24} />
         </div>
       </div>
     </div>
