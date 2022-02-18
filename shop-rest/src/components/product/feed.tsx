c9795003f81c04bebaa20bb2dc14bbc65f08dc18
vinender singh <46550815+vinender@users.noreply.github.com>
2022-02-18 12:54:43 +0530

    draft products

--- shop-rest/src/components/product/feed.tsx
+++ shop-rest/src/components/product/feed.tsx
@@ -78,7 +78,7 @@ const Feed = ({ shopId }: { shopId: string }) => {
           <>
             {data?.pages.map((products, _idx) => (
               <Fragment key={_idx}>
-                {products?.data?.map((product) => (
+                {products.data.filter(product => product?.status === 'publish').map(product => (
                   <motion.div key={product.id}>
                     {renderProductCard(product)}
                   </motion.div>
