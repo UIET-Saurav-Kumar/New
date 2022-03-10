35b3bb5fd6293d10c63f82a65b03703c28f1c068
vinender <vinendersingh91@gmail.com>
2022-03-10 18:21:23 +0530

    logs

--- admin-rest/src/components/logs/log-list.tsx
+++ admin-rest/src/components/logs/log-list.tsx
@@ -13,46 +13,46 @@ export type IProps = {
 const LogList = ({ logs, onPagination }: IProps) => {
   const { t } = useTranslation();
   const { data, paginatorInfo } = logs!;
-  const rowExpandable = (record: any) => record.children?.length;
+  const rowExpandable = (record: any) => record?.children?.length;
 
   const { alignLeft } = useIsRTL();
   function generateData(data:any){
-    if(data.type=="order"){
-      return `order ${data.order.tracking_number} created`
+    if(data?.type=="order"){
+      return `order ${data?.order?.tracking_number} created`
     }
-    if(data.type=="item-added"){
-      return `${data.products} is added in cart`
+    if(data?.type=="item-added"){
+      return `${data?.products} is added in cart`
     }
-    if(data.type=="item-removed"){
-      return `${data.products} is removeed from cart`
+    if(data?.type=="item-removed"){
+      return `${data?.products} is removeed from cart`
     }
-    if(data.type=="search_item"){
-      return data.search_item+" is Searched"
+    if(data?.type=="search_item"){
+      return data?.search_item+" is Searched"
     }
   }
   function formateDate(data:any)
   {
-    var time=data.created_at.split('T')[1];
-    var date=data.created_at.split('T')[0].split('-');
+    var time=data?.created_at.split('T')[1];
+    var date=data?.created_at.split('T')[0].split('-');
     return date[2]+"/"+date[1]+"/"+date[0]+"  "+time.split('.')[0]
   }
   function formateType(data:any){
-    if(data.type=="order"){
+    if(data?.type=="order"){
       return "Order created"
     }
-    if(data.type=="item-removed"){
+    if(data?.type=="item-removed"){
       return "Item removed form cart"
     }
-    if(data.type=="item-added"){
+    if(data?.type=="item-added"){
       return "Item added in cart"
     }
-    if(data.type=="search_item"){
+    if(data?.type=="search_item"){
       return "Item searched"
     }
-    if(data.type=="location"){
+    if(data?.type=="location"){
       return "Location searched"
     }
-    return data.type
+    return data?.type
   }
 
   const columns = [
@@ -72,12 +72,12 @@ const LogList = ({ logs, onPagination }: IProps) => {
     {
       title: ("User"),
       align: alignLeft,
-      render: (data:any) => (data.user)?data.user.name:"---",
+      render: (data:any) => (data?.user)?data?.user?.name:"---",
     },
     {
       title: ("Phone no."),
       align: alignLeft,
-      render: (data:any) => (data.user)?data.user.phone_number:"---",
+      render: (data:any) => (data?.user)?data?.user?.phone_number:"---",
     },
     {
       title: ("Data"),
@@ -93,7 +93,7 @@ const LogList = ({ logs, onPagination }: IProps) => {
     {
       title: ("Shop"),
       align: alignLeft,
-      render: (data:any) => (data.shop)?data.shop.name:"---",
+      render: (data:any) => (data?.shop)?data?.shop?.name:"---",
     },
     {
       title: ("Timestamp"),
@@ -132,12 +132,12 @@ const LogList = ({ logs, onPagination }: IProps) => {
         />
       </div>
 
-      {!!paginatorInfo.total && (
+      {!!paginatorInfo?.total && (
         <div className="flex justify-end items-center">
           <Pagination
-            total={paginatorInfo.total}
-            current={paginatorInfo.currentPage}
-            pageSize={paginatorInfo.perPage}
+            total={paginatorInfo?.total}
+            current={paginatorInfo?.currentPage}
+            pageSize={paginatorInfo?.perPage}
             onChange={onPagination}
           />
         </div>
