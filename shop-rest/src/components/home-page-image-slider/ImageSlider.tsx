b67d3c0918dda7544c3d6a0ba3be67280c828499
vinender <vinendersingh91@gmail.com>
2022-04-05 15:10:11 +0530

    events/mags

--- shop-rest/src/components/home-page-image-slider/ImageSlider.tsx
+++ shop-rest/src/components/home-page-image-slider/ImageSlider.tsx
@@ -85,7 +85,7 @@ export default function ImageSlider() {
 
         <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
 
-            <img className="object-fill"
+            <img className="object-fill h-full w-full"
             //  className='rounded-md w-full opacity-90  h-40 lg:w-full lg:h-72 xl+:h-80 md:h-64  2xl:h-72 2xl:w-full space-x-9 object-fill object '  
                   src={'/banner/community.jpg'} 
                   />
@@ -95,7 +95,7 @@ export default function ImageSlider() {
 
         <div className="card flex relative w-full h-40 lg:h-72 xl+:h-80 2xl:h-72 md:h-64 ">
 
-            <img className="object-fill"
+            <img className="object-fill h-full w-full"
             //  className='rounded-md w-full opacity-90  h-40 lg:w-full lg:h-72 xl+:h-80 md:h-64  2xl:h-72 2xl:w-full space-x-9 object-fill object '  
                   src={'/banner/shop-community.jpg'} 
                   />
