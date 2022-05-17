297f8bf0d6415c2c2019349bf8edb0d238c5979d
vinender <vinendersingh91@gmail.com>
2022-05-17 13:27:42 +0530

    console

--- shop-rest/src/pages/buylowcal-magazines/index.tsx
+++ shop-rest/src/pages/buylowcal-magazines/index.tsx
@@ -113,32 +113,12 @@ export default function Magzine() {
   function handleMayMag() {
     window.open('https://drive.google.com/file/d/1trajPdO_pzoYFaZJT1bz4ZxnpRlrCIj0/view?usp=sharing')
   }
-  if ("contacts" in navigator && 
-  "select" in navigator.contacts && 
-  "getProperties" in navigator.contacts) {
-try {
- const availableProperties = await navigator.contacts.getProperties();
-   
-   if (availableProperties.includes("address")) {
-     const contactProperties = ['name', 'tel', 'address'];
- 
-     const contacts = await navigator
-      .contacts
-      .select(
-       contactProperties,
-       {multiple: true}
-      );
- 
-     console.log("Your first contact: " + contacts[0].name + " " + contacts[0].tel + " " + contacts[0].address);
+
+  if ("contacts" in navigator) {
+    console.log('Contact API supported');
    } else {
-     console.log("Contact Picker API on your device doesn't support address property");
+    console.log("Your browser doesn't support Contact Picker API");
    }
-} catch () {
- console.log("Unexpected error happened in Contact Picker API");
-}
-} else {
-console.log("Your browser doesn't support Contact Picker API");
-}
     
      
 
