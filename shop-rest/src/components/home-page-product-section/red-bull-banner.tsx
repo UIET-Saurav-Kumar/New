4e988ed6033d7af64699eb08b7395975694ed4aa
vinender singh <46550815+vinender@users.noreply.github.com>
2022-05-31 18:37:13 +0530

    Add files via upload

---
+++ shop-rest/src/components/home-page-product-section/red-bull-banner.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Link from 'next/link';
+import { useCategoriesQuery } from "@data/home/use-categories-query";
+import { useRouter } from "next/router";
+import { useUI } from "@contexts/ui.context";
+import { useModalAction } from "@components/ui/modal/modal.context";
+
+
+export default function RedBullBanner() {
+
+    const router = useRouter();
+	const { query } = useRouter();
+    const { isAuthorize } = useUI();
+    const { openModal } = useModalAction();
+
+
+    const { type } = query;
+
+    function getLink() {
+		
+           isAuthorize ? router.push('/user/invite') :
+            openModal("REGISTER")
+
+	}
+
+    return (
+
+   
+   <div onClick={getLink} className='flex w-full mt-8 md:mt-10 h-auto '>   
+            <img src='/red-bull.jpg' 
+                className=' md:h-auto object-contain cursor-pointer pt-0 lg:w-full -sm-pt-60 
+                            lg:object-contain lg:mt-8 xl:object-cover'/>
+            
+   </div> 
+       
+    )
+}
