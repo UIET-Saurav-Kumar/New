a8fcac963d13f4e38482c4d1d54001e7e7a3f13b
vinender <vinendersingh91@gmail.com>
2022-06-11 12:18:11 +0530

    otp detect

--- shop-rest/src/components/auth/forget-password/enter-token-view.tsx
+++ shop-rest/src/components/auth/forget-password/enter-token-view.tsx
@@ -43,7 +43,7 @@ const EnterTokenView = ({ onSubmit, loading }: Props) => {
         })
         .catch((err) => {
           ac.abort();
-          alert(err);
+          console.log(err);
         });
     }
   }, []);
