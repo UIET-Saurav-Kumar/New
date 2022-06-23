21d358559e51dd404f45b3fbf7353c76057352ca
vinender <vinendersingh91@gmail.com>
2022-06-23 14:22:18 +0530

    interakt

--- api/packages/pickbazar-shop/src/Database/Repositories/OrderRepository.php
+++ api/packages/pickbazar-shop/src/Database/Repositories/OrderRepository.php
@@ -274,8 +274,8 @@ class OrderRepository extends BaseRepository
                     "event"=> "Order Placed Successfully",
                     "traits"=> [
                         "productDetail"=> json_encode($request->products),
-                         'shop_name'=> $product->shop->name,
-                        'product_name'=> $product->name,
+                        'shop_name'=> $product->shop->name,
+                        'product_name'=> $products->name,
                         "price"=> $request->amount,
                         "orderId"=> $request->tracking_number,
                         "delivery_time"=> $request->delivery_time,
