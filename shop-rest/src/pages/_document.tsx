9208fc3089a4a6c2e3fb566d5e85b3925095cc61
vinender singh <46550815+vinender@users.noreply.github.com>
2022-08-07 18:33:09 +0530

    gtag manager

--- shop-rest/src/pages/_document.tsx
+++ shop-rest/src/pages/_document.tsx
@@ -50,7 +50,7 @@ export default class CustomDocument extends Document {
               }}
             />
 
-<meta name="facebook-domain-verification" content="8mvkzpangtng356xvg4hqubj4mbuyq" />
+          <meta name="facebook-domain-verification" content="8mvkzpangtng356xvg4hqubj4mbuyq" />
 
            {/* interakt */}
           <script
@@ -71,6 +71,19 @@ export default class CustomDocument extends Document {
                 kiwi.init('', 'R74PF9xANjTtlgpYCm6WFEpmXvX7ZJ42', {});`,
               }}
             />
+
+             {/* Google tag manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-58NV2K4'`
+            }}
+            />
+
+            
           
           
           <script
@@ -114,6 +127,8 @@ export default class CustomDocument extends Document {
         </Head>
        
         <body dir={dir}>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58NV2K4"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
         </body>
