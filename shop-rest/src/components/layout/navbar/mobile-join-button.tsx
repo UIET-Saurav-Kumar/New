d194e4402315bcf194ee573b2c024a6b26cb30b3
vinender <vinendersingh91@gmail.com>
2022-08-12 14:04:18 +0530

    update

--- shop-rest/src/components/layout/navbar/mobile-join-button.tsx
+++ shop-rest/src/components/layout/navbar/mobile-join-button.tsx
@@ -4,15 +4,17 @@ import { useModalAction } from "@components/ui/modal/modal.context";
 import { useTranslation } from "next-i18next";
 
 export default function MobileJoinButton() {
+
   const { t } = useTranslation("common");
   const { openModal } = useModalAction();
   
   function handleJoin() {
-    return openModal("OTP_LOGIN");
+    return openModal("REGISTER");
   }
+
   return (
-    <Button className="font-semibold  " size="small" onClick={handleJoin}>
-      {t("join-button")}
+    <Button className="font-semibold" size="small" onClick={handleJoin}>
+      {t("Join")}
     </Button>
   );
 }
