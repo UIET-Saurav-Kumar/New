823ed4921d24a9aba124ac2cb466fe8aebc96941
vinender <vinendersingh91@gmail.com>
2022-12-13 13:06:51 +0530

    gpay

--- shop-rest/src/pages/payment-apps.tsx
+++ shop-rest/src/pages/payment-apps.tsx
@@ -5,7 +5,7 @@ export default function PaymentApps() {
     function handleClick() {
         if (navigator.userAgent.match(/android/i) && navigator.userAgent.match(/google pay/i)) {
             // If the Google Pay app is installed, redirect to the app using a deep link URL
-            window.location = "https://pay.google.com/gp/v/home";
+            window.location = "com.google.android.apps.nbu.paisa.user";
           } else {
             // If the app is not installed, redirect to the Google Play Store to download it
             window.location = "https://play.google.com/store/apps/details?id=com.google.android.apps.walletnfcrel";
