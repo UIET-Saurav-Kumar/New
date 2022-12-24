9832aa6f24c3ff0b464073689be4f0df5b923111
vinender <vinendersingh91@gmail.com>
2022-12-24 15:23:57 +0530

    linkedin icon

--- shop-rest/src/components/icons/social/index.tsx
+++ shop-rest/src/components/icons/social/index.tsx
@@ -2,4 +2,4 @@ export { FacebookIcon } from "./facebook";
 export { InstagramIcon } from "./instagram";
 export { TwitterIcon } from "./twitter";
 export { YouTubeIcon } from "./youtube";
-export {LinkedIn} from  './linkedin';
+export {LinkedInIcon} from  './linkedin';
