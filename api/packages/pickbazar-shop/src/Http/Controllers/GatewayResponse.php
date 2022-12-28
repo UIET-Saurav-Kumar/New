9f4fe8c53a7813d64db83c56c32368932b024263
vinender <vinendersingh91@gmail.com>
2022-12-28 15:27:00 +0530

    updates

--- api/packages/pickbazar-shop/src/Http/Controllers/GatewayResponse.php
+++ api/packages/pickbazar-shop/src/Http/Controllers/GatewayResponse.php
@@ -149,7 +149,7 @@ class GatewayResponse extends CoreController
           $utility_payment=UtilityPayment::where('tracking_number', $order_id)->first();
           $code=$this->recharge($utility_payment);
       
-          $recharge_status = $this->rechargeStatus($utility_payment->trans_id);
+          $recharge_status = $this->rechargeStatus($utility_payment->order_id);
           $txid = $recharge_status[0];
           $status = $recharge_status[1];
           $error_code = $recharge_status[2];
@@ -162,7 +162,7 @@ class GatewayResponse extends CoreController
             UtilityPayment::where('tracking_number', $order_id)->update(['status' => 'FAILED']);
           }
       
-           $callback_url = "https://buylowcal.com/callback?status=Success&txid=TransID&mytxid=aPITransID&optxid=OPeratorID&mobileno=Mobile";
+           $callback_url = "https://buylowcal.com/callback?status=Success&txid=.$order_id.&mytxid=.$txid.&optxid=OPeratorID&mobileno=Mobile";
       
           return redirect()->away($callback_url);
       
