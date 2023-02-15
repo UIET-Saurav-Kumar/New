1ae8a914862affe0c18ede2e57628e75608c3efc
vinender <vinendersingh91@gmail.com>
2023-02-15 16:13:24 +0530

    hotjar new script

--- shop-rest/src/pages/_document.tsx
+++ shop-rest/src/pages/_document.tsx
@@ -193,7 +193,7 @@ export default class CustomDocument extends Document {
         /> */}
          
          {/* Mouse flow */}
-        <script 
+        {/* <script 
         type="text/javascript"
         dangerouslySetInnerHTML={{
                               __html:`(
@@ -205,7 +205,22 @@ export default class CustomDocument extends Document {
             document.getElementsByTagName("head")[0].appendChild(mf);
           })();
           `}}
-          />
+          /> */}
+
+          
+          {/* hotjar new */}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html:`(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:3368072,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
+        `,}}
+        />
 
         
 
