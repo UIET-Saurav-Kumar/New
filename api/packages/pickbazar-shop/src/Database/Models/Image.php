4d538665edcdbe477c246ffec11b644e084ff573
vinender <vinendersingh91@gmail.com>
2023-04-26 17:04:08 +0530

    image upload update

--- api/packages/pickbazar-shop/src/Database/Models/Image.php
+++ api/packages/pickbazar-shop/src/Database/Models/Image.php
@@ -4,6 +4,7 @@ namespace PickBazar\Database\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Image extends Model
 
